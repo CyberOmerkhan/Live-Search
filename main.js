2f9188da6a6efb7188d3172f39d9c21848232b5d
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-10 18:22:33 +0600

    alpha version

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    let input = $('#input');
+    input.on('input', function() {
+        let value = $(this).val();
+        let items = document.querySelectorAll('.ul li');
+        if(value){
+            items.forEach((elem) => {
+                if(elem.innerText.search(value) == -1){
+                    elem.classList.add('hide');
+                }
+            })
+        }else{
+            items.forEach(elem => {
+                elem.classList.remove('hide');
+            })
+        }
+    })
+})
